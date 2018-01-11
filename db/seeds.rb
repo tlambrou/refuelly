4d32c89ea946b082732263f4ffb38dcb8064a326
Tassos <lambrou@gmail.com>
2018-01-10 20:47:51 -0800

    Added seed file that will inject 1,000,000 cafes with slightly different coordinates

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Cafe.connection.execute("insert into cafes (name, latitude, longitude, created_at, updated_at) (
+    select 'Cafe ' || i as name, 39 + x.lat as latitude, -76 - x.lon as longitude, x.date as created_at, x.update as updated_at
+    from (
+      select i, random() * 10 as lat, random() * 10 as lon, CURRENT_TIMESTAMP as date, CURRENT_TIMESTAMP as update
+      from generate_series(1,1000000) as i
+    )
+    as x );")
